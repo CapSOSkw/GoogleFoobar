65d12fbe3f2a888d59fee00edbda2ff15baee76c
CapSOSkw <33869007+CapSOSkw@users.noreply.github.com>
2018-01-28 12:49:17 -0500

    Update find_the_access_codes.py

--- find_the_access_codes.py
+++ find_the_access_codes.py
@@ -3,7 +3,7 @@ def myanswer(l):
     list_size = len(l)
     count = 0
 
-    for i in range(1, len(l)-1):
+    for i in range(1, len(l) - 1):
 
         # print(i,l[i], l[:i], l[i+1:])
         front = [item for item in l[:i] if l[i] % item == 0]
@@ -11,6 +11,10 @@ def myanswer(l):
         behind = [item for item in l[i+1:] if item % l[i] == 0]
 
         # print(front, behind)
-        count = count + len(front)*len(behind)
+        count = count + len(front) * len(behind)
 
     return count
+
+
+
+print myanswer([1,2,3,4,5,6])
