c18463994380ad3643f0bad9e560a0b7ed1e2dca
CapSOSkw <33869007+CapSOSkw@users.noreply.github.com>
2018-01-28 12:18:06 -0500

    Create minion_task_scheduling.py

---
+++ minion_task_scheduling.py
@@ -0,0 +1,24 @@
+def answer(data, n):
+    # your code here
+    count_jobs_dict = [(i, data.count(i)) for i in data]
+
+    job_index = [i[0] for i in count_jobs_dict]
+    job_count = [i[1] for i in count_jobs_dict]
+    # max_count = max(job_count)
+    max_count = -1
+    for i in job_count:
+        if i > max_count:
+            max_count = i
+
+    if n == 0:
+        return []
+
+    result = []
+    for i, j in enumerate(job_count):
+        if j <= n:
+            result.append(job_index[i])
+
+    return result
+
+
+print(answer([5,10,15,10,7], 1))
