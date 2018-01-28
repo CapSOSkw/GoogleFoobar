847286bb4990834ad9b0283e46be92e9bd11e368
CapSOSkw <33869007+CapSOSkw@users.noreply.github.com>
2018-01-28 12:45:50 -0500

    Update En_Route_Salute.py

--- En_Route_Salute.py
+++ En_Route_Salute.py
@@ -2,11 +2,11 @@ def answer(x):
 
     index_less = [index for index, i in enumerate(x) if i=="<"]
     index_more = [index for index, i in enumerate(x) if i==">"]
-
     count = 0
+    
     for i in index_less:
         for j in index_more:
-            if i>j:
+            if i > j:
                 count += 2
             else:
                 continue
