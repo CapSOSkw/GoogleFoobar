ef87cfd40e9852a28a641169bd76d8023378985f
CapSOSkw <33869007+CapSOSkw@users.noreply.github.com>
2018-01-28 13:04:59 -0500

    Update lovely_lucky_lambs.py

--- lovely_lucky_lambs.py
+++ lovely_lucky_lambs.py
@@ -1,9 +1,10 @@
 from math import sqrt, log
 
-
 def answer(total_lambs):
+    
     total_lambs = int(total_lambs)
-    if 10 <= total_lambs <= 10 ** 9:
+    
+    if 10 <= total_lambs <= 10 ** 9:    
         geo_seq_sum = 0
         geo_seq_n = 1
 
@@ -14,10 +15,12 @@ def answer(total_lambs):
                 break
 
         geo_seq_sum = sum(geo_seq(geo_seq_n))
+        
         remainder = total_lambs - geo_seq_sum
         if remainder >= 2 ** (geo_seq_n - 1) + 2 ** (geo_seq_n - 2):
             geo_seq_n += 1
 
+        # =========    
         fib_sum = 0
         fib_n = 1
 
@@ -40,10 +43,8 @@ def geo_seq(n):
         yield a
         a, b = b, 2 * b
 
-
 def sum_geo_seq(n):
     return (2 ** n) - 1
-
 
 def Fib(n):
     return 1 if n == 1 else int((1 + sqrt(5)) ** n - (1 - sqrt(5)) ** n) / (2 ** n * sqrt(5))
