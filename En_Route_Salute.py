9f8a5f3a305b3e04a799df7acdde6d802a9f5b2f
CapSOSkw <33869007+CapSOSkw@users.noreply.github.com>
2018-01-28 11:58:29 -0500

    Create En_Route_Salute.py

---
+++ En_Route_Salute.py
@@ -0,0 +1,18 @@
+def answer(x):
+
+    index_less = [index for index, i in enumerate(x) if i=="<"]
+    index_more = [index for index, i in enumerate(x) if i==">"]
+
+    count = 0
+    for i in index_less:
+        for j in index_more:
+            if i>j:
+                count += 2
+            else:
+                continue
+
+    return count
+
+
+print(answer(">>--<<>><<"))
+
